52b3efeb61a3ad29eaa728e3158342b2bdd3b673
peter6888 <qq1800@gmail.com>
2018-07-31 20:31:31 -0700

    finished week 3 exercise
    Finished week 3 exercise

--- ex3-objsets/src/main/scala/objsets/TweetSet.scala
+++ ex3-objsets/src/main/scala/objsets/TweetSet.scala
@@ -41,7 +41,7 @@ abstract class TweetSet {
    * Question: Can we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def filter(p: Tweet => Boolean): TweetSet = ???
+    def filter(p: Tweet => Boolean): TweetSet = filterAcc(p, new Empty)
   
   /**
    * This is a helper method for `filter` that propagetes the accumulated tweets.
@@ -54,7 +54,7 @@ abstract class TweetSet {
    * Question: Should we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def union(that: TweetSet): TweetSet = ???
+    def union(that: TweetSet): TweetSet
   
   /**
    * Returns the tweet from this set which has the greatest retweet count.
@@ -65,7 +65,9 @@ abstract class TweetSet {
    * Question: Should we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def mostRetweeted: Tweet = ???
+    def mostRetweeted: Tweet = mostRetweetedAcc(new Tweet("ini_max", "ini_max", 0))
+
+    def mostRetweetedAcc(most: Tweet): Tweet
   
   /**
    * Returns a list containing all tweets of this set, sorted by retweet count
@@ -76,7 +78,7 @@ abstract class TweetSet {
    * Question: Should we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def descendingByRetweet: TweetList = ???
+    def descendingByRetweet: TweetList
   
   /**
    * The following methods are already implemented
@@ -107,7 +109,7 @@ abstract class TweetSet {
 }
 
 class Empty extends TweetSet {
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
+    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = acc
   
   /**
    * The following methods are already implemented
@@ -120,11 +122,21 @@ class Empty extends TweetSet {
   def remove(tweet: Tweet): TweetSet = this
 
   def foreach(f: Tweet => Unit): Unit = ()
+
+  override def union(that: TweetSet): TweetSet = that
+
+  override def mostRetweetedAcc(most: Tweet): Tweet = most
+  override def descendingByRetweet: TweetList = Nil
 }
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
+    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = {
+      if(p(elem))
+        left.filterAcc(p, right.filterAcc(p, acc.incl(elem)))
+      else
+        left.filterAcc(p, right.filterAcc(p, acc))
+    }
   
     
   /**
@@ -151,6 +163,19 @@ class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
     f(elem)
     left.foreach(f)
     right.foreach(f)
+  }
+
+  override def union(that: TweetSet): TweetSet = {
+    (left union (right union that)).incl(elem)
+  }
+
+  override def mostRetweetedAcc(max: Tweet): Tweet = {
+    if (elem.retweets > max.retweets) right.mostRetweetedAcc(left.mostRetweetedAcc(elem))
+    else right.mostRetweetedAcc(left.mostRetweetedAcc(max))
+  }
+  
+  override def descendingByRetweet: TweetList = {
+    new Cons(this.mostRetweeted, this.remove(this.mostRetweeted).descendingByRetweet)
   }
 }
 
